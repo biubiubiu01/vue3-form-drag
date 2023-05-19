305f3dee5e5e3be395f0d9ad8960baf1d084f1ce
biubiubiu01 <1979420758@qq.com>
2023-05-19 10:29:38 +0800

    chore(custom): test
    哈哈哈
    222

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,10 +2,34 @@
  * @Author: lzy
  * @Date: 2023-05-18 17:11:36
  * @LastEditors: lzy
- * @LastEditTime: 2023-05-19 10:00:09
+ * @LastEditTime: 2023-05-19 10:26:12
  * @FilePath: \vue3-form-drag\commitlint.config.cjs
  *
  */
 module.exports = {
-    extends: ["cz"]
+    extends: ["@commitlint/config-conventional", "cz"],
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            [
+                "feat", // 新功能
+                "fix", // 修复bug
+                "refactor", // 重构
+                "docs", // 文档
+                "chore", // 增加依赖或库
+                "style", // 格式（不影响代码变动）
+                "revert", // 撤销commit 回滚上一版本
+                "perf", // 性能优化
+                "test" // 测试单元
+            ]
+        ],
+        "type-case": [0],
+        "type-empty": [0],
+        "scope-empty": [0],
+        "scope-case": [0],
+        "subject-full-stop": [0, "never"],
+        "subject-case": [0, "never"],
+        "header-max-length": [0, "always", 72]
+    }
 };
