bc060d0e03dd8604cd3a11beeccff2946f293aab
biubiubiu01 <1979420758@qq.com>
2023-05-19 09:48:41 +0800

    feat: test
    tttt

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,11 @@
+/*
+ * @Author: lzy
+ * @Date: 2023-05-18 17:11:36
+ * @LastEditors: lzy
+ * @LastEditTime: 2023-05-18 18:10:34
+ * @FilePath: \vue3-form-drag\commitlint.config.cjs
+ *
+ */
 module.exports = {
     extends: ["@commitlint/config-conventional"],
     rules: {
